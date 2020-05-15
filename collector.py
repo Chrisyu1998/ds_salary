094310ff0a128812bd9396d582931da658d08281
Chris Yu <chrisyu0620@gmail.com>
2020-05-15 10:55:03 +0800

    upload the data

--- collector.py
+++ collector.py
@@ -1,1 +1,1 @@
-import glassdoor_scraper as gsimport pandas as pdpath = "/Users/cyt/Downloads/ds_salary/chromedriver"df = gs.get_jobs('software engineer',500,False,path,30)
+import glassdoor_scraper as gsimport pandas as pdpath = "/Users/cyt/Downloads/ds_salary/chromedriver"df = gs.get_jobs('software engineer',500,False,path,30)df.to_csv('glassdoor_jobs.csv',index = False)
